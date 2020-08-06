429c78e7563c4fdc741da027de1688fce24790c4
river <1376754470@qq.com>
2020-08-06 18:12:43 +0800

    ribbon

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -4,6 +4,7 @@ import com.atguigu.springcloud.enetities.CommonResult;
 import com.atguigu.springcloud.enetities.Payment;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,5 +31,16 @@ public class OrderController {
     @GetMapping("/consumer/payment/getPaymentById/{id}")
     public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id){
         return restTemplate.getForObject(PAYMENT_URL+"/payment/getPaymentById/"+id,CommonResult.class);
+    }
+
+    @GetMapping("/consumer/payment/getForEntity/{id}")
+    public CommonResult<Payment> getPayment(@PathVariable("id") Long id){
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/payment/getPaymentById/" + id, CommonResult.class);
+        log.info("*******************************"+entity.getHeaders()+"*********************************");
+        if (entity.getStatusCode().is2xxSuccessful()){ // 2xx表示http请求的状态响应码
+            return entity.getBody(); // 返回一个请求体内容
+        }else {
+            return new CommonResult<>(500,"请求失败",null);
+        }
     }
 }
