b689ee1ab3419ef6691843acf31ae11f1996cd51
river <1376754470@qq.com>
2020-08-20 18:02:57 +0800

    p85 3:08

---
+++ cloud-config-center-3366/src/main/java/com/atguigu/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @auther River
+ * @date 2020/8/20 15:37
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class,args);
+    }
+}
---
+++ cloud-config-center-3366/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,26 @@
+package com.atguigu.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @auther River
+ * @date 2020/8/20 15:41
+ */
+@RestController
+@Slf4j
+@RefreshScope // 自动刷新，检测到config server的变化
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+
+        return configInfo;
+    }
+}
